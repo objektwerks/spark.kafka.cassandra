c6c83dc9838e1e4a0d40b0ac270a30d25cd10fd1
objektwerks <objektwerks@runbox.com>
2016-12-05 08:39:14 -0500

    refactored spark conf

--- src/test/scala/spark/SparkInstance.scala
+++ src/test/scala/spark/SparkInstance.scala
@@ -5,7 +5,12 @@ import org.apache.spark.{SparkConf, SparkContext}
 import scala.io.Source
 
 object SparkInstance {
-  val conf = new SparkConf().setMaster("local[2]").setAppName("sparky").set("spark.cassandra.connection.host", "127.0.0.1")
+  val conf = new SparkConf(loadDefaults = true)
+    .setMaster("local[2]")
+    .setAppName("sparky")
+    .set("spark.cassandra.connection.host", "127.0.0.1")
+    .set("spark.cassandra.auth.username", "cassandra")
+    .set("spark.cassandra.auth.password", "cassandra")
   val context = new SparkContext(conf)
   val license = Source.fromInputStream(getClass.getResourceAsStream("/license.mit")).getLines.toSeq
 }
