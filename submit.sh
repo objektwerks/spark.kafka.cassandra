026657812623c2d5d2bf599d05e3a1cbfc84b10d
objektwerks <objektwerks@runbox.com>
2019-08-04 08:30:46 -0400

    removed blank lines form submit.sh

--- submit.sh
+++ submit.sh
@@ -4,5 +4,3 @@ spark-submit \
   --master local[2] \
   --packages org.apache.spark:spark-streaming-kafka-0-10:2.4.3,org.apache.kafka:kafka:2.3.0,com.datastax.spark:spark-cassandra-connector:2.4.1,com.typesafe:config:1.3.4 \
   ./target/scala-2.11/spark-kafka-cassandra_2.11-0.1.jar
-
-
